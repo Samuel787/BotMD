c4ea56bbb07ef855bdc9a94984cc30b78fdda074
samuel787 <suther.david.samuel@gmail.com>
2020-06-19 23:25:29 +0800

    added the listview fragment

--- app/src/main/java/com/example/botmdtest/MainActivity.java
+++ app/src/main/java/com/example/botmdtest/MainActivity.java
@@ -2,16 +2,22 @@ package com.example.botmdtest;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_temperature);
+        setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.custom_toolbar);
         TextView textView = getSupportActionBar().getCustomView().findViewById(R.id.toolbar_title);
--- app/src/main/java/com/example/botmdtest/TemperatureFragment.java
+++ app/src/main/java/com/example/botmdtest/TemperatureFragment.java
@@ -1,13 +1,20 @@
 package com.example.botmdtest;
 
 
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -25,7 +32,48 @@ public class TemperatureFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_temperature, container);
+        Log.v("Meowww", "Oncreate view");
+        View rootView = inflater.inflate(R.layout.fragment_temperature, container);
+        Button btn = rootView.findViewById(R.id.locationButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("meow", "meow");
+            }
+        });
+        return rootView;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.v("Meowww", "meow");
+        selectLocation();
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        selectLocation();
+    }
+
+    private void selectLocation(){
+        Log.v("Lel", "test");
+        Button btn = getView().findViewById(R.id.locationButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragMgr = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragTrans = fragMgr.beginTransaction();
+
+                //TemperatureFragment myFragment = new TemperatureFragment(); //my custom fragment
+                LocationFragment myFragment = new LocationFragment();
+                fragTrans.add(R.id.fragmentContainer, myFragment);
+                fragTrans.addToBackStack(null);
+                fragTrans.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                fragTrans.commit();
+            }
+        });
+    }
 }
